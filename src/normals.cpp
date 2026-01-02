49659a4f796c3eecf9306ae6b11bd9d8a7cdd313
kjun627 <kjun627@naver.com>
2026-01-02 14:12:14 +0900

    rendering bunny by Ray tracing

--- src/normals.cpp
+++ src/normals.cpp
@@ -1,28 +1,26 @@
 #include <nori/integrator.h>
+#include <nori/scene.h>
 
 NORI_NAMESPACE_BEGIN
 
 class NormalIntegrator : public Integrator {
 public:
     NormalIntegrator(const PropertyList &props){
-        m_myProperty = props.getString("myProperty");
-        std::cout << "parameter value was : " << m_myProperty << std::endl;
+        // m_myProperty = props.getString("myProperty");
+        // std::cout << "parameter value was : " << m_myProperty << std::endl;
     }
 
     Color3f  Li(const Scene *scene, Sampler *sampler, const Ray3f &ray) const{
-        return Color3f(0,1,0);
+        Intersection its;
+        if(!scene->rayIntersect(ray, its)) return Color3f(0.0f);
+        
+        Normal3f n = its.shFrame.n.cwiseAbs();
+        return Color3f(n.x(), n.y(), n.z());
     }
 
     std::string toString() const{
-        return tfm::format(
-            "NoramlIntegrator[\n"
-        " myProperty = \"%s\"\n"
-        "]",
-        m_myProperty
-        );
+        return "NormalIntegrator[]";
     }
-protected:
-    std::string m_myProperty;
 };
 
 NORI_REGISTER_CLASS(NormalIntegrator, "normals");
