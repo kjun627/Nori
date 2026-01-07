aa0663ec13f8665440a7a5dc0602ca86f6b901cd
kjun627 <kjun627@naver.com>
2026-01-07 12:19:09 +0900

    feat add: SimpleIntegrator with shadowRay

--- src/simple.cpp
+++ src/simple.cpp
@@ -11,9 +11,20 @@ public:
     Color3f Li(const Scene* scene, Sampler* sampler, const Ray3f& ray)const{
         Intersection its;
         if(!scene->rayIntersect(ray,its)) return Color3f(0.0f);
+                
         Point3f dist = m_position - its.p;
         float distSquare = dist.squaredNorm();
         Vector3f lightDir = dist.normalized();
+        
+        Ray3f shadowRay(its.p,lightDir);
+        shadowRay.mint = Epsilon;
+        shadowRay.maxt = dist.norm();
+        Intersection itsForShadow;
+        if(scene->rayIntersect(shadowRay,its)){
+            return Color3f(0.0f);
+        }
+        
+
         float cosTheta = its.shFrame.n.dot(lightDir);
 
         float reflecMulDist = 1/(distSquare * 4 * pow(M_PI,2));
